1550632ec9d4c68a54083cbc0cd7de6a9062749e
Beverly Appleton <bfcain19@gmail.com>
2020-04-01 04:18:34 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,16 +1,9 @@
-def my_collect(array)
-  i = 0
-  while i < array.length
-    yield(array[i])
-    i = i + 1
-  end
-end
 def my_collect(collection)
   i = 0
-  collection = []
-  while i < languages.upcase
-    collection << yield (array[i])
-    i +=1
+  new_collection = []
+  while i < collection.length
+    new_collection << yield(collection[i])
+    i += 1
   end
-  collection
+  new_collection
 end
